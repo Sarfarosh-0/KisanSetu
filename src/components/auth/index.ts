227aa3bc60ce8b6c4dafa70266c7a422f74d8d7b
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-09-18 12:26:59 +0530

    fixed stuff

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,7 +1,7 @@
 // AgriMarket Auth Components Re-exports
 export * from "./LoginPage";
 export * from "./AuthLayout";
-export * from "./BrandPanel";
+// export * from "./BrandPanel";
 export * from "./RoleSelector";
 export * from "./PhoneLoginForm";
 export * from "./OtpVerification";
